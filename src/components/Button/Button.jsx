73aa15abda94708f4cdcbdca8fd17fdc7fdba920
foxyuh <arthurbryansk@gmail.com>
2022-05-25 23:29:50 -0300

    adicionado sons ao clicar nos botões

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+import ButtonAudio from '../../assets/sounds/mixkit-single-classic-click-1116.wav'
 import './Button.css'
 
 function Button({ children, active, HandleAddTime, HandleSetAppStyle, HandleSetPomodoroDivStyle, HandleSetStartButtonStyle, HandleSetActiveButtonStyle, HandleSetProgressStyle }) {
+
+    const audio = new Audio(ButtonAudio)
+
+    const startAudio = () => {
+    audio.play()
+    }
 
     return (
         <button 
         className={`button ${active}`}
         onClick={() => {
+            startAudio()
             HandleAddTime()
             HandleSetAppStyle()
             HandleSetPomodoroDivStyle()
--- src/components/start-button/StartButton.jsx
+++ src/components/start-button/StartButton.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
+import ReactAudioPlayer from 'react-audio-player';
+import StartButtonAudio from '../../assets/sounds/mixkit-mouse-click-close-1113.wav'
 import './StartButton.css';
 
 function StartButton({ children, className, HandleStartButtonClick, HandleStartOrStop }) {
+
+    const audio = new Audio(StartButtonAudio)
+
+    const startAudio = () => {
+    audio.play()
+    }
 
     return (
         <button 
         className={`start-button ${className}`}
         onClick={() => {
+            startAudio()
             HandleStartButtonClick()
             HandleStartOrStop()
         }}
